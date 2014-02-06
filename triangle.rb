8f250bb9b6b5ff350a1e8a4ef2be74c2ef24f919
randydinh <randy@househappy.org>
2014-02-06 14:32:09 -0800

    2_6_2014

--- triangle.rb
+++ triangle.rb
@@ -13,27 +13,23 @@
 # and
 #   about_triangle_project_2.rb
 #
-def triangle(a, b, c)
-  	if (a == b) and (b == c) 
-  	  return :equilateral
-  	elsif (a == b) or (a == c) or (b == c)
-   	  return :isosceles
-  	else 
-  	  return :scalene
-end
-end
 
-# def triangle(a, b, c)
-#   if ((a == b) && (a == c) && (b == c))
-#     return :equilateral
-#   elsif ((a == b) || (a == c) || (b == c))
-#     return :isosceles
-#   else
-#     return :scalene
-#   end
-# end
+
+def triangle(a, b, c)
+	raise TriangleError if a <= 0 || b <= 0 || c <= 0	
+	raise TriangleError if a + b <= c || b + c <= a || a + c <= b	
+
+	if ((a == b) && (a == c) && (b == c))
+    	return :equilateral
+	elsif ((a == b) || (a == c) || (b == c))
+    	return :isosceles
+	else
+    	return :scalene
+ 	end
+end
 
 
 # Error class used in part 2.  No need to change this code.
-# class TriangleError < StandardError
-# end
+
+class TriangleError < StandardError
+end
